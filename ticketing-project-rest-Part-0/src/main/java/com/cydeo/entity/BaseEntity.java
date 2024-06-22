6f84425e02cd35d78272d549f3a3d07ef6adaaf2
mng2214 <mng2214@gmail.com>
2024-06-22 17:05:33 -0500

    2.7 spring change

--- ticketing-project-rest-Part-0/src/main/java/com/cydeo/entity/BaseEntity.java
+++ ticketing-project-rest-Part-0/src/main/java/com/cydeo/entity/BaseEntity.java
@@ -3,7 +3,7 @@ package com.cydeo.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
+
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
