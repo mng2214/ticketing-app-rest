249baf2bd3160ca98abe11fe435ca2b210b2bd8b
mng2214 <mng2214@gmail.com>
2024-06-21 21:40:14 -0500

    task controller added

--- ticketing-project-rest-Part-0/src/main/java/com/cydeo/dto/UserDTO.java
+++ ticketing-project-rest-Part-0/src/main/java/com/cydeo/dto/UserDTO.java
@@ -16,10 +16,13 @@ public class UserDTO {
     private String firstName;
     private String lastName;
     private String userName;
+
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String passWord;
+
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String confirmPassWord;
+
     private boolean enabled;
     private String phone;
     private RoleDTO role;
